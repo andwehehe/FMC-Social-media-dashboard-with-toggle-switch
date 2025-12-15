f977c872dd8d74ac3c395db3e542ff6478002ad6
Andrei Gobrs <andreigobres3@gmail.com>
2025-12-15 18:47:14 +0800

    fixed error 404

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FMC-Social-media-dashboard-with-theme-switch/',
+  base: '/FMC-Social-media-dashboard-with-toggle-switch/',
   plugins: [react()],
 })
