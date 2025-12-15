245df32306d69306f248b46a446d3cdb9e75beda
Andrei Gobrs <andreigobres3@gmail.com>
2025-12-15 18:44:16 +0800

    fixed accessibility error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FMC-Social-media-dashboard-with-theme-switcher/',
+  base: '/FMC-Social-media-dashboard-with-theme-switch/',
   plugins: [react()],
 })
